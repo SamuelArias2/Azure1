5668ae9816c0f29d300c0581bcf831433875a3c4
SamuelArias2 <166361317+SamuelArias2@users.noreply.github.com>
2024-04-12 17:43:33 -0600

    Add files via upload

---
+++ SysEducaConnect.DAL/BDContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SysEducaConnect.EN;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace SysEducaConnect.DAL
+{
+    public class BDContext:DbContext
+    {
+        public DbSet<Rol> Rol { get; set; }
+        public DbSet<Usuario> Usuario { get; set; }
+        public DbSet<Registro> Registro { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=SAMUEL_ARIAS\\SQLEXPRESS;Initial Catalog=EducaConnect4;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        }
+    }
+}
